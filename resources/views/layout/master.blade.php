e562830f1da24e78464a1bcd735b4c67e40ba604
ahhzaky <siahmadzaky@gmail.com>
2021-05-30 14:47:47 +0700

    Dashboard view

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -18,35 +18,40 @@
     <link href="/assets/css/theme.css" rel="stylesheet" />
     <title>@yield('title')</title>
 </head>
-<body><nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 backdrop" data-navbar-on-scroll="data-navbar-on-scroll">
-    <div class="container"><a class="navbar-brand d-flex align-items-center fw-semi-bold fs-3" href="#"> <img class="me-3" src="/assets/img/gallery/logo.png" alt="" />
-        <div class="text-primary font-base">List BukuMu</div>
-      </a>
-      <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-      <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
-        <ul class="navbar-nav ms-auto pt-2 pt-lg-0 font-base">
-          <li class="nav-item"><a class="nav-link fw-medium active" aria-current="page" href="#">Home</a></li>
-          <li class="nav-item"><a class="nav-link" href="">Books</a></li>
-          <li class="nav-item"><a class="nav-link" href="">Libraries</a></li>
-          <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">More</a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action </a></li>
-            </ul>
-          </li>
-        </ul>
-        <form class="ps-lg-5">
-          <button class="btn btn-lg btn-primary rounded-pill bg-gradient font-base order-0" type="submit">Login</button>
-        </form>
-      </div>
-    </div>
-  </nav>
+<body>
+
+
   <main class="main" id="top">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 backdrop" data-navbar-on-scroll="data-navbar-on-scroll">
+        <div class="container"><a class="navbar-brand d-flex align-items-center fw-semi-bold fs-3" href="#"> <img class="me-3" src="assets/img/gallery/logo.png" alt="" />
+            <div class="text-primary font-base">List BukuMu</div>
+          </a>
+          <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+          <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto pt-2 pt-lg-0 font-base">
+              <li class="nav-item"><a class="nav-link fw-medium active" aria-current="page" href="#">Home</a></li>
+              <li class="nav-item"><a class="nav-link" href="#books">List Favorite</a></li>
+
+              <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">More</a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Profile</a></li>
+                  <li><a class="dropdown-item" href="#">Buat list baru</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="ps-lg-5">
+              <button class="btn btn-lg btn-primary rounded-pill bg-gradient font-base order-0" type="submit">Login</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+
     @yield("container")
   </main>
 
   {{-- javascript --}}
-  <script src="/vendors/@popperjs/popper.min.js"></script>
+     <script src="/vendors/@popperjs/popper.min.js"></script>
     <script src="/vendors/bootstrap/bootstrap.min.js"></script>
     <script src="/vendors/is/is.min.js"></script>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
